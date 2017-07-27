c86136baf695c4095e703215ab93e786e752bc18
Daniel Götten <daniel.goetten@gmail.com>
2017-07-27 12:18:23 +0200

    License added.

--- hikari-support/src/main/scala/de/dangoe/freda/hikari/HikariConnectionProvider.scala
+++ hikari-support/src/main/scala/de/dangoe/freda/hikari/HikariConnectionProvider.scala
@@ -1,3 +1,18 @@
+/**
+  * Copyright 2017 Daniel Götten
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package de.dangoe.freda.hikari
 
 import java.sql.Connection
@@ -8,10 +23,6 @@ import de.dangoe.freda.{ConnectionProvider, ConnectionSettings, ReadOnly}
 
 import scala.concurrent.{ExecutionContext, Future, blocking}
 
-/**
-  * @author Daniel Götten <daniel.goetten@fashionid.de>
-  * @since 27.07.17
-  */
 class HikariConnectionProvider private[freda](config: HikariConfig) extends ConnectionProvider {
 
   private val dataSource = createDataSource(config)
